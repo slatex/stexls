67b4fc6f0cb83d5ba040df82e8276381abe22cdc
Marian Plivelic <MarianPlivelic@gmail.com>
2019-05-25 17:40:13 +0200

    only write if so specified and changed

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ def database(path):
     from trefier.misc import Cache
     with Cache(path, DatabaseCLI) as cache:
         @arg('path', help="Location of the cache")
-        def write_cache(path:str):
+        def write_cache(path):
             cache.write(path)
         def delete_cache():
             cache.delete()
         def cache_path():
             return cache.path
         cache.data.run(write_cache, delete_cache, cache_path)
-        cache.write_on_exit = cache.data.changed
-        print("[Exit] Database cache changed?", cache.data.changed)
+        cache.write_on_exit = cache.data.changed and cache.write_on_exit
 
 dispatch_commands([model, database])

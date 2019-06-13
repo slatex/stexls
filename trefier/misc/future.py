7aa2f0da2a00dbc4272a51158e3afd0aa91debb1
Marian Plivelic <MarianPlivelic@gmail.com>
2019-06-13 12:12:43 +0200

    doc

--- trefier/misc/future.py
+++ trefier/misc/future.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Callable, Any
 import threading
 import sys
+import traceback
 
 class Future:
     def __init__(self, task: Callable[[], Any], response_timeout: float = 1.0):
@@ -15,17 +16,16 @@ class Future:
         self._return_value = None
         self._return_value_handled = threading.Event()
         self._did_raise_exception = False
-        self._exception = None
+        self._traceback: traceback.TracebackType = None
         self._exception_handled = threading.Event()
         self._task_thread = threading.Thread(target=self._task_container, args=(task,))
         self._task_thread.start()
     
-    def done(self, callback: Callable[[Any], None], catch: Optional[Callable[[Exception], None]] = None):
+    def done(self, callback: Callable[[Any], None], catch: Optional[Callable[[traceback.TracebackType], None]] = None):
         """ Registers a callback for the return value and a callback for raised exceptions
         :param callback: A callback that expects a single argument,
             that is set to the return value of the task if it returns
-        :param catch: A callback wich expects the an exception as argument and is thrown in case
-            the task raises an exception.
+        :param catch: A callback wich accepts a traceback as argument and is called in case the task raises an exception
         """
         assert callback is not None
         callback_thread = threading.Thread(
@@ -34,34 +34,46 @@ class Future:
         callback_thread.start()
     
     def _task_container(self, task):
+        """ Contains the provided task and notifies all registered callbacks. """
         try:
             self._return_value = task()
-        except Exception as e:
+        except:
             self._did_raise_exception = True
-            self._exception = e
+            self._traceback = traceback.format_exc()
         self._notify()
     
     def _notify(self):
+        """ Notifies all callbacks that the task returned or raised an exception """
         self._finished.set()
         if self._response_timeout is not None:
             if self._did_raise_exception:
                 if not self._exception_handled.wait(self._response_timeout):
+                    print(self._traceback)
                     print(f"Exception of future not handled withing {self._response_timeout} second", file=sys.stderr)
-                    print(self._exception, file=sys.stderr)
             else:
                 if not self._return_value_handled.wait(self._response_timeout):
                     print(f"Return value of future not handled within {self._response_timeout} second", file=sys.stderr)
     
     def _callback_container(self, callback, catch):
+        """ Thread added by every callback and executed after the task resolves or throws. """
         self._finished.wait()
         try:
             if self._did_raise_exception:
                 if catch is not None:
                     self._exception_handled.set()
-                    catch(self._exception)
+                    catch(self._traceback)
             else:
                 self._return_value_handled.set()
                 callback(self._return_value)
-        except Exception as e:
-            print("Exception thrown inside future callback", file=sys.stderr)
-            print(e, file=sys.stderr)
+        except:
+            traceback.print_exc()
+            if self._did_raise_exception:
+                print("Exception thrown inside future catch()", file=sys.stderr)
+            else:
+                print("Exception thrown inside future callback()", file=sys.stderr)
+
+def make_async(f):
+    """ Decorator that wraps the function to always return a future object. """
+    def wrapper(*args, **kwargs):
+        return Future(lambda: f(*args, **kwargs))
+    return wrapper

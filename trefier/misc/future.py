03fc80dbf17b5f34d1742f75b181f85ea329e25f
Marian Plivelic <MarianPlivelic@gmail.com>
2019-06-14 11:36:31 +0200

    then improvements

--- trefier/misc/future.py
+++ trefier/misc/future.py
@@ -1,22 +1,24 @@
 from __future__ import annotations
-from typing import Callable, Any, List
+from typing import Callable, Any, List, Optional
 import threading
 import sys
 import traceback
 
 class Future:
-    def __init__(self, task: Callable[[], Any], response_timeout: float = 1.0):
+    def __init__(self, task: Callable[[], Any], response_timeout: float = 1.0, check_result_handled: bool = True):
         """ Creates a thread for the provided task.
             The return value can be retrieved by registering a callback using Future.done()
         :param task: Callable that returns something
         :param response_timeout: Maximum time the return value or raised exception waits until a warning is printed
         """
+        self._require_result_handling = check_result_handled
         self._response_timeout = response_timeout
         self._finished = threading.Event()
-        self._return_value = None
+        self._return_value: Any = None
         self._return_value_handled = threading.Event()
         self._did_raise_exception = False
-        self._traceback: traceback.TracebackType = None
+        self._traceback: Optional[str] = None
+        self._exception_raised: Optional[Exception] = None
         self._exception_handled = threading.Event()
         self._callback_threads: List[threading.Thread] = []
         self._lock = threading.RLock()
@@ -32,6 +34,7 @@ class Future:
     def join(self):
         """ Waits until all callbacks have been resolved """
         with self._lock:
+            self._task_thread.join()
             while self._callback_threads:
                 thread = self._callback_threads.pop()
                 thread.join()
@@ -60,33 +63,40 @@ class Future:
     def then(self, callback, catch=None):
         with self._lock:
             event = threading.Event()
-            result_ptr = [None]
-            traceback_ptr = [None]
+            result_ptr = []
+            tb_ptr = []
 
             def _resolve_task(result):
-                result_ptr[0] = result
-                event.set()
+                try:
+                    result_ptr.append(callback(result))
+                finally:
+                    event.set()
 
-            def _catch_task(tb):
-                traceback_ptr[0] = tb
-                event.set()
+            def _catch_task(exception, tb):
+                try:
+                    tb_ptr.append(tb)
+                    if catch is not None:
+                        catch(exception, tb)
+                finally:
+                    event.set()
 
             self.done(_resolve_task, _catch_task)
 
             def _wait_task():
                 event.wait()
-                if traceback_ptr[0] is not None:
-                    raise Exception(traceback_ptr[0])
-                return result_ptr[0]
+                if result_ptr:
+                    return result_ptr[0]
+                raise Exception(tb_ptr[0])
 
-            return Future(_wait_task).done(callback, catch)
+            return Future(_wait_task, check_result_handled=False)
 
     def _task_container(self, task):
         """ Contains the provided task and notifies all registered callbacks. """
         try:
             self._return_value = task()
-        except:
+        except Exception as e:
             self._did_raise_exception = True
+            self._exception_raised = e
             self._traceback = traceback.format_exc()
         self._notify()
     
@@ -98,7 +108,7 @@ class Future:
                 if not self._exception_handled.wait(self._response_timeout):
                     print(self._traceback)
                     print(f"Exception of future not handled withing {self._response_timeout} second", file=sys.stderr)
-            else:
+            elif self._require_result_handling:
                 if not self._return_value_handled.wait(self._response_timeout):
                     print(f"Return value of future not handled within {self._response_timeout} second", file=sys.stderr)
     
@@ -109,7 +119,7 @@ class Future:
             if self._did_raise_exception:
                 if catch is not None:
                     self._exception_handled.set()
-                    catch(self._traceback)
+                    catch(self._exception_raised, self._traceback)
             else:
                 self._return_value_handled.set()
                 callback(self._return_value)

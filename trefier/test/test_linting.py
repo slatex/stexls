d0778ecc15bf26ad96c5740a4106b6a751f35697
Marian Plivelic <MarianPlivelic@gmail.com>
2019-06-14 16:37:42 +0200

    update

--- trefier/test/test_linting.py
+++ trefier/test/test_linting.py
@@ -20,13 +20,11 @@ class LinterDocumentTestCase(unittest.TestCase):
 
     def test_module_symbol_count(self):
         symbols = list(self._make_module_document().symis)
-        symbol_count = len(symbols)
-        self.assertEquals(symbol_count, 5)
+        self.assertEquals(len(symbols), 5)
 
     def test_module_gimport_count(self):
         imports = list(self._make_module_document().gimports)
-        import_count = len(imports)
-        self.assertEquals(import_count, 4)
+        self.assertEquals(len(imports), 4)
 
     def test_binding_compile_success(self):
         document = self._make_binding_document()
@@ -37,34 +35,37 @@ class LinterDocumentTestCase(unittest.TestCase):
     def test_binding_definition(self):
         document = self._make_binding_document()
         self.assertIsNotNone(document.binding, "Binding expected")
-        self.assertEqual(str(document.module_identifier), "testdb/all_symbol_types/test_module")
+        self.assertEqual(str(document.module_identifier), "testdb/all_symbol_types/module")
         self.assertEqual(str(document.binding.lang), "lang")
 
     def test_binding_trefi_count(self):
         trefis = list(self._make_binding_document().trefis)
-        trefi_count = len(trefis)
-        self.assertEqual(trefi_count, 12)
+        self.assertEqual(len(trefis), 12)
 
     def test_binding_defi_count(self):
         defis = list(self._make_binding_document().defis)
-        defi_count = len(defis)
-        self.assertEqual(defi_count, 10)
+        self.assertEqual(len(defis), 10)
 
     @staticmethod
     def _make_module_document():
-        return Document('trefier/test/testdb/all_symbol_types/source/module.tex', False)
+        return Document('trefier/test/testdb/all_symbol_types/source/module.tex')
 
     @staticmethod
     def _make_binding_document():
-        return Document('trefier/test/testdb/all_symbol_types/source/module.lang.tex', False)
+        return Document('trefier/test/testdb/all_symbol_types/source/module.lang.tex')
 
 
 class TestImportGraph(unittest.TestCase):
-    def test_open_in_image_viewer(self):
+    def _setup(self):
         linter = Linter()
-        linter.add_directory('trefier/test/testdb/repo2/source')
-        linter.update(use_multiprocessing=False)
-        linter.import_graph.open_in_image_viewer('testdb/repo2/module1')
+        linter.add_directory('trefier/test/testdb/simple/source')
+        self.assertEqual(linter.update(use_multiprocessing=False), 3)
+        self.assertTrue(not linter.exceptions)
+        return linter
+
+    def test_open_in_image_viewer(self):
+        linter = self._setup()
+        linter.import_graph.open_in_image_viewer('testdb/simple/module1')
 
 
 class TestLinter(unittest.TestCase):
@@ -75,12 +76,12 @@ class TestLinter(unittest.TestCase):
         self.assertTrue(not linter.exceptions)
         self.assertEqual(len(linter.ls), 5)
         self.assertEqual(len(linter.modules), 2)
-        self.assertEqual(len([e for el in linter.symbols.values() for e in el]), 3)
+        self.assertEqual(len(linter.symbols), 3)
         self.assertEqual(len(linter.bindings), 3)
         self.assertEqual(len(linter.trefis), 3)
         self.assertEqual(len(linter.defis), 3)
 
-    def test_remove_all(self):
+    def test_unlink_all(self):
         linter = Linter()
         linter.add_directory('trefier/test/testdb/repo3/source')
         self.assertEqual(linter.update(use_multiprocessing=False), 5)
@@ -100,22 +101,57 @@ class TestLinter(unittest.TestCase):
         self.assertEqual(len(linter.bindings), 0)
         self.assertEqual(len(linter.trefis), 0)
         self.assertEqual(len(linter.defis), 0)
+        self.assertDictEqual(linter._map_file_to_document, {})
+        self.assertDictEqual(linter._map_module_identifier_to_bindings, {})
+        self.assertDictEqual(linter._map_module_identifier_to_module, {})
+        self.assertDictEqual(linter.failed_to_parse, {})
+        self.assertDictEqual(linter.exceptions, {})
+
+    def test_link_unlink(self):
+        file = 'trefier/test/testdb/all_symbol_types/source/module.tex'
+        document = Document(file)
+        self.assertTrue(document.success)
+        linter = Linter()
+        self.assertFalse(linter._is_linked(file))
+        linter._link(document)
+        self.assertTrue(linter._is_linked(file))
+        linter._unlink(file)
+        self.assertFalse(linter._is_linked(file))
+        linter._link(document)
+        self.assertTrue(linter._is_linked(file))
+
+    def test_symbol_positions(self):
+        linter = Linter()
+        linter.add_directory('trefier/test/testdb/two_peaks/source')
+        self.assertEqual(linter.update(use_multiprocessing=False), 16)
+        self.assertTrue(not linter.exceptions)
+
+    class TestThreadsafeLinter(Linter):
+        def lock_reader(self, delay):
+            from time import sleep
+            with self._rwlock.reader():
+                sleep(delay)
+
+        def lock_writer(self, delay):
+            from time import sleep
+            with self._rwlock.writer():
+                sleep(delay)
 
     def test_threadsafe_read(self):
-        linter = Linter()
+        linter = TestLinter.TestThreadsafeLinter()
 
         time_a = time()
-        linter.lock_reader(1, 'a')
-        linter.lock_reader(1, 'b')
+        linter.lock_reader(1)
+        linter.lock_reader(1)
         delta_a = time() - time_a
 
         self.assertAlmostEqual(delta_a, 2, 2)
 
-        f1 = Future(lambda: linter.lock_reader(1, 'c'))
-        f1.done(ignore)
-        f2 = Future(lambda: linter.lock_reader(1, 'd'))
-        f2.done(ignore)
+        f1 = Future(lambda: linter.lock_reader(1)).done(ignore)
+        f2 = Future(lambda: linter.lock_reader(1)).done(ignore)
+
         time_b = time()
+
         f1.join()
         f2.join()
 
@@ -123,19 +159,18 @@ class TestLinter(unittest.TestCase):
         self.assertAlmostEqual(time() - time_b, 1, 2)
 
     def test_threadsafe_write(self):
-        linter = Linter()
+        linter = TestLinter.TestThreadsafeLinter()
 
         time_a = time()
-        linter.lock_writer(1, 'a')
-        linter.lock_writer(1, 'b')
+        linter.lock_writer(1)
+        linter.lock_writer(1)
         delta_a = time() - time_a
 
         self.assertAlmostEqual(delta_a, 2, 2)
 
-        f1 = Future(lambda: linter.lock_writer(1, 'c'))
-        f1.done(ignore)
-        f2 = Future(lambda: linter.lock_writer(1, 'd'))
-        f2.done(ignore)
+        f1 = Future(lambda: linter.lock_writer(1)).done(ignore)
+        f2 = Future(lambda: linter.lock_writer(1)).done(ignore)
+
         time_b = time()
         f1.join()
         f2.join()

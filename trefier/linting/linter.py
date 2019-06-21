cef2c8f6a2c2e2618d84f250c25ae5f564e28471
Marian Plivelic <MarianPlivelic@gmail.com>
2019-06-21 13:16:00 +0200

    async decorators

--- trefier/linting/linter.py
+++ trefier/linting/linter.py
@@ -7,7 +7,7 @@ import multiprocessing
 
 from trefier.misc.location import *
 from trefier.misc.file_watcher import FileWatcher
-from trefier.misc.rwlock import RWLock
+from trefier.misc.rwlock import RWLock, async_writer, async_reader
 from trefier.models import seq2seq
 
 from trefier.linting.exceptions import *
@@ -29,124 +29,152 @@ class Linter(FileWatcher):
         else:
             raise LinterInternalException.create(path, 'Unable to load tagger model')
 
-    @property
-    def modules(self):
-        return [str(module.module) for _, module in self._map_module_identifier_to_module.items()]
+    @async_writer(lambda self: self._rwlock)
+    def write(self, delay):
+        from time import sleep
+        sleep(delay)
+        return 'write done after %f' % delay
 
-    @property
+    @async_reader(lambda self: self._rwlock)
+    def read(self, delay):
+        from time import sleep
+        sleep(delay)
+        return 'read done after %f' % delay
+
+    @async_reader(lambda self: self._rwlock)
+    def ls(self):
+        return list(self._map_file_to_document)
+
+    @async_reader(lambda self: self._rwlock)
+    def modules(self):
+        return [
+            str(document.module)
+            for module, document
+            in self._map_module_identifier_to_module.items()
+        ]
+
+    @async_reader(lambda self: self._rwlock)
+    def bindings(self):
+        return [
+            str(document.binding)
+            for module, bindings
+            in self._map_module_identifier_to_bindings.items()
+            for lang, document in bindings.items()
+        ]
+
+    @async_reader(lambda self: self._rwlock)
     def defis(self):
         return [
-            f'{str(defi)} "{defi.text}"'
+            str(defi)
             for module, bindings
             in self._map_module_identifier_to_bindings.items()
             for lang, document in bindings.items()
             for defi in document.defis
         ]
 
-    @property
+    @async_reader(lambda self: self._rwlock)
     def trefis(self):
-        return [
-            f'{str(trefi)} "{trefi.text}"'
-            for module, bindings
-            in self._map_module_identifier_to_bindings.items()
-            for lang, document in bindings.items()
-            for trefi in document.trefis
-        ]
+        with self._rwlock.reader():
+            return [
+                str(trefi)
+                for module, bindings
+                in self._map_module_identifier_to_bindings.items()
+                for lang, document in bindings.items()
+                for trefi in document.trefis
+            ]
 
-    @property
-    def bindings(self):
-        return list(f'{module}.{lang}'
-                    for module, bindings
-                    in self._map_module_identifier_to_bindings.items()
-                    for lang in bindings)
-
-    @property
-    def ls(self):
-        return list(self._map_file_to_document)
-
-    @property
+    @async_reader(lambda self: self._rwlock)
     def symbols(self):
-        return list(
-            f'{module}/{sym.symbol_name}'
+        return [
+            str(symi)
             for module, document
             in self._map_module_identifier_to_module.items()
-            for sym in document.symis
-        )
+            for symi in document.symis
+        ]
 
-    def module_at_position(self, file, line, column):
-        with self._rwlock.reader():
-            doc: Document = self._map_file_to_document.get(file)
-            if doc is None:
-                raise Exception("File not tracked currently")
+    @async_reader(lambda self: self._rwlock)
+    def module_at_position(self, file: str, line: int, column: int):
+        doc = self._map_file_to_document.get(file)
+        if doc is None:
+            raise Exception("File not tracked")
 
-            position = Position(line, column)
+        position = Position(line, column)
 
-            for trefi in doc.trefis:
-                if trefi.target_module_location is not None:
-                    if trefi.target_module_location.range.contains(position):
-                        yield trefi.symbol_name
+        if doc.module is not None:
+            if doc.module.range.contains(position):
+                return doc.module.module_name
 
-            for gimport in doc.gimports:
-                if gimport.imported_module_location.range.contains(position):
-                    yield str(gimport.imported_module)
+        if doc.binding is not None:
+            if doc.binding.range.contains(position):
+                return doc.binding.bound_module_name
 
+        for trefi in doc.trefis:
+            if trefi.target_module_location is not None:
+                if trefi.target_module_location.range.contains(position):
+                    return str(trefi.target_module)
+
+        for gimport in doc.gimports:
+            if gimport.imported_module_location.range.contains(position):
+                return str(gimport.imported_module)
+        return None
+
+    @async_writer(lambda self: self._rwlock)
     def add_directory(self, directory):
-        with self._rwlock.writer():
-            added = 0
-            for d in glob(directory, recursive=True):
-                if os.path.isdir(d):
-                    if d not in self._watched_directories:
-                        self._watched_directories.append(d)
-                        added += 1
-            return added
+        added = 0
+        for d in glob(directory, recursive=True):
+            if os.path.isdir(d):
+                if d not in self._watched_directories:
+                    self._watched_directories.append(d)
+                    added += 1
+        return added
 
+    @async_writer(lambda self: self._rwlock)
     def update(self, n_jobs=None, debug=False, use_multiprocessing=True):
-        with self._rwlock.writer():
-            # update watched directories
-            for d in list(self._watched_directories):
-                if not os.path.isdir(d):
-                    # remove if no longer valid directory
-                    self._watched_directories.remove(d)
-                else:
-                    # else add all direct files inside it
-                    self.add(f'{d}/*')
-
-            # update watched file index
-            deleted, modified = super().update()
-            if not (modified or deleted):
-                return 0
-
-            for file in itertools.chain(deleted, modified):
-                try:
-                    if self._is_linked(file):
-                        self._unlink(file)
-                except Exception as e:
-                    self.exceptions.setdefault(file, [])
-                    self.exceptions[file].append(e)
-
-            # Parse all files in parallel or sequential
-            if use_multiprocessing:
-                with multiprocessing.Pool(n_jobs) as pool:
-                    documents = pool.map(Document, modified)
+        # update watched directories
+        for d in list(self._watched_directories):
+            if not os.path.isdir(d):
+                # remove if no longer valid directory
+                self._watched_directories.remove(d)
             else:
-                documents = list(map(Document, modified))
+                # else add all direct files inside it
+                self.add(f'{d}/*')
 
-            for failed_document in filter(lambda doc: not doc.success, documents):
-                self.failed_to_parse[failed_document.file] = failed_document.exceptions
+        # update watched file index
+        deleted, modified = super().update()
+        if not (modified or deleted):
+            return 0
 
-            for document in filter(lambda doc: doc.success, documents):
-                if document.exceptions:
-                    self.exceptions.setdefault(document.file, [])
-                    self.exceptions[document.file].extend(document.exceptions)
-                try:
-                    self._link(document)
-                except Exception as e:
-                    self.exceptions.setdefault(document.file, [])
-                    self.exceptions[document.file].append(e)
-            
-            self.import_graph.update()
+        for file in itertools.chain(deleted, modified):
+            try:
+                if self._is_linked(file):
+                    self._unlink(file)
+            except Exception as e:
+                self.exceptions.setdefault(file, [])
+                self.exceptions[file].append(e)
 
-            return len(documents)
+        # Parse all files in parallel or sequential
+        if use_multiprocessing:
+            with multiprocessing.Pool(n_jobs) as pool:
+                documents = pool.map(Document, modified)
+        else:
+            documents = list(map(Document, modified))
+
+        for failed_document in filter(lambda doc: not doc.success, documents):
+            self.failed_to_parse[failed_document.file] = failed_document.exceptions
+
+        for document in filter(lambda doc: doc.success, documents):
+            if document.exceptions:
+                self.exceptions.setdefault(document.file, [])
+                self.exceptions[document.file].extend(document.exceptions)
+            try:
+                self._link(document)
+            except Exception as e:
+                self.exceptions.setdefault(document.file, [])
+                self.exceptions[document.file].append(e)
+
+        self.import_graph.update()
+
+        return len(documents)
 
     def _is_linked(self, file: str) -> bool:
         return self._map_file_to_document.get(file) is not None

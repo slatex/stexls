2298c1adf8222469eef27f081f3c3621d53a6452
Marian Plivelic <MarianPlivelic@gmail.com>
2019-08-12 21:16:57 +0200

    simplifications & keep parser
    Former-commit-id: 03bc69156e346c475ccf3fa34946f5b91c6a7a18

--- trefier/linting/document.py
+++ trefier/linting/document.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Dict, Iterator
+from typing import Optional, Dict, Iterator, Union
 import re
 from functools import partial
 import sys
@@ -32,31 +32,59 @@ class Document:
     @property
     def exception_summary(self) -> Iterator[Tuple[Range, str]]:
         yield from self.exceptions
-        if hasattr(self, 'trefis'):
-            for trefi in self.trefis:
-                yield from map((lambda x: (trefi.range, x)), trefi.errors)
-        if hasattr(self, 'defis'):
-            for defi in self.defis:
-                yield from map((lambda x: (defi.range, x)), defi.errors)
-        if hasattr(self, 'symis'):
-            for symi in self.symis:
-                yield from map((lambda x: (symi.range, x)), symi.errors)
-        if hasattr(self, 'module') and self.module:
+        for trefi in self.trefis or ():
+            yield from map((lambda x: (trefi.range, x)), trefi.errors)
+        for defi in self.defis or ():
+            yield from map((lambda x: (defi.range, x)), defi.errors)
+        for symi in self.symis or ():
+            yield from map((lambda x: (symi.range, x)), symi.errors)
+        if self.module:
             yield from map((lambda x: (self.module.range, x)), self.module.errors)
-        if hasattr(self, 'binding') and self.binding:
+        if self.binding:
             yield from map((lambda x: (self.binding.range, x)), self.binding.errors)
+    
+    def __getstate__(self):
+        return (
+            self.file,
+            self.exceptions,
+            self.module,
+            self.binding,
+            self.syntax_errors,
+            self.success,
+            self.symis,
+            self.gimports,
+            self.trefis,
+            self.defis)
+    
+    def __setstate__(self, state):
+        self.parser = None
+        (self.file,
+         self.exceptions,
+         self.module,
+         self.binding,
+         self.syntax_errors,
+         self.success,
+         self.symis,
+         self.gimports,
+         self.trefis,
+         self.defis) = state
 
     def __init__(self, file: str, ignore_exceptions: bool = True):
         self.file = file
         self.exceptions: List[Tuple[Range, Exception]] = []
+        self.parser: Optional[LatexParser] = None
         self.module: Optional[ModuleDefinitonSymbol] = None
         self.binding: Optional[ModuleBindingDefinitionSymbol] = None
         self.syntax_errors: Optional[Dict[Location, Dict[str, object]]] = None
         self.success = False
+        self.symis = None
+        self.gimports = None
+        self.trefis = None
+        self.defis = None
         try:
-            parser = LatexParser(file, ignore_exceptions=False)
-            self.syntax_errors = parser.syntax_errors
-            if parser.success:
+            self.parser = LatexParser(self.file, ignore_exceptions=False)
+            self.syntax_errors = self.parser.syntax_errors
+            if self.parser.success:
                 def catcher(symbol_type_constructor):
                     def wrapper(node):
                         try:
@@ -70,34 +98,38 @@ class Document:
 
                 self.symis: List[SymiSymbol] = list(
                     filter(None, map(catcher(SymiSymbol.from_node),
-                                     parser.root.finditer(SymiSymbol.SYM_PATTERN))))
+                                     self.parser.root.finditer(SymiSymbol.SYM_PATTERN))))
 
                 self.gimports: List[GimportSymbol] = list(
                     filter(None, map(catcher(GimportSymbol.from_node),
-                                     parser.root.finditer(GimportSymbol.GIMPORT_PATTERN))))
+                                     self.parser.root.finditer(GimportSymbol.GIMPORT_PATTERN))))
 
                 self.trefis: List[TrefiSymbol] = list(
                     filter(None, map(catcher(TrefiSymbol.from_node),
-                                     parser.root.finditer(TrefiSymbol.TREFI_PATTERN))))
+                                     self.parser.root.finditer(TrefiSymbol.TREFI_PATTERN))))
 
                 self.defis: List[DefiSymbol] = list(
                     filter(None, map(catcher(DefiSymbol.from_node),
-                                     parser.root.finditer(DefiSymbol.DEFI_PATTERN))))
+                                     self.parser.root.finditer(DefiSymbol.DEFI_PATTERN))))
 
                 modules: List[ModuleDefinitonSymbol] = list(
                     filter(None, map(catcher(ModuleDefinitonSymbol.from_node),
-                                     parser.root.finditer(ModuleDefinitonSymbol.MODULE_PATTERN))))
+                                     self.parser.root.finditer(ModuleDefinitonSymbol.MODULE_PATTERN))))
 
                 if len(modules) >= 1:
                     self.module = modules[0]
 
                 bindings: List[ModuleBindingDefinitionSymbol] = list(
                     filter(None, map(catcher(ModuleBindingDefinitionSymbol.from_node),
-                                     parser.root.finditer(ModuleBindingDefinitionSymbol.MODULE_BINDING_PATTERN))))
+                                     self.parser.root.finditer(ModuleBindingDefinitionSymbol.MODULE_BINDING_PATTERN))))
 
                 if len(bindings) >= 1:
                     self.binding = bindings[0]
+                
+                if self.binding is not None and self.module is not None:
+                    raise LinterException(f'File must not contain binding and module definition!')
 
-                self.success = self.binding != self.module
+                self.success = True
+
         except Exception as e:
             self.exceptions.append((Range(Position()), e))

5c9db02e703e95a950947a4f8303679ffc7d9f72
Marian Plivelic <MarianPlivelic@gmail.com>
2019-09-09 13:23:14 +0200

    throw on invalid roman
    Former-commit-id: 111b424a28ae9ab87ba52f9f95d673c24ad857ec

--- trefier/misc/roman_numerals.py
+++ trefier/misc/roman_numerals.py
@@ -2,14 +2,8 @@ from typing import Optional
 
 _ROMAN_NUMERALS = ('i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x', 'xi', 'xii')
 
-def int2roman(i: int) -> Optional[str]:
-    try:
-        return _ROMAN_NUMERALS[i - 1]
-    except:
-        return None
+def int2roman(i: int) -> str:
+    return _ROMAN_NUMERALS[i - 1]
 
-def roman2int(r: str) -> Optional[int]:
-    try:
-        return _ROMAN_NUMERALS.index(r) + 1
-    except:
-        return None
+def roman2int(r: str) -> int:
+    return _ROMAN_NUMERALS.index(r) + 1

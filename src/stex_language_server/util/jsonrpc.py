732eff712be65d2e98c4e306dada5190331c8466
Marian Plivelic <MarianPlivelic@gmail.com>
2019-12-08 22:35:46 +0100

    progress
    Former-commit-id: 191b64ec1c4b445aa377d59fadf5171c40c09e6d

--- src/stex_language_server/util/jsonrpc.py
+++ src/stex_language_server/util/jsonrpc.py
@@ -2,11 +2,12 @@
 Specification from: https://www.jsonrpc.org/specification#overview
 """
 from __future__ import annotations
-from typing import Optional, Union, List, Dict
+from typing import Optional, Union, List, Dict, Callable
+from concurrent.futures import thread
 import json
-import threading
 import http
 import socket
+import functools
 from enum import IntEnum
 
 __all__ = [
@@ -40,7 +41,7 @@ class RequestMessage(Message):
         The server must repond with a ResponseMessage containing
         the same id the client provided.
     '''
-    def __init__(self, id: Union[str, int], method: str, params: Union[Dict, List]):
+    def __init__(self, id: Union[str, int], method: str, params: Union[Dict[str, Any], List[Any]]):
         ''' Initializes the request object.
         Parameters:
             id: Client defined identifier used to re-identify the response.
@@ -64,7 +65,7 @@ class NotificationMessage(Message):
         The server will try to execute the method but the client will not be notified
         about the results.
     '''
-    def __init__(self, method: str, params: Union[Dict, List]):
+    def __init__(self, method: str, params: Union[Dict[str, Any], List[Any]]):
         ''' Initializes a notification message.
             See RequestMessage for information about parameters.
         '''
@@ -77,7 +78,7 @@ class ResponseMessage(Message):
     ''' A response message gives success or failure status in response
         to a request message with the same id.
     '''
-    def __init__(self, id: Union[str, int], result: Optional[object] = None, error: Optional[ErrorObject] = None):
+    def __init__(self, id: Union[str, int], result: Optional[Any] = None, error: Optional[ErrorObject] = None):
         ''' Initializes a response message.
         Parameters:
             id: The id of the request to respond to.
@@ -99,7 +100,7 @@ class ResponseMessage(Message):
 
 class ErrorObject:
     " Gives more information in case a request method can't be executed by the server. "
-    def __init__(self, code: int, message: str, data: Optional[object] = None):
+    def __init__(self, code: int, message: str, data: Optional[Any] = None):
         ''' Constructs error object.
         Parameters:
             code: A number that indicates the error type occured.
@@ -129,3 +130,106 @@ class ErrorCodes(IntEnum):
     # ServerError = -32000 to -32099 (implementation defined)
     FailedToSerializeJson = -32000
 
+
+class ByteBuffer:
+    def __init__(self, maxsize: int = 1):
+        self.buffer = b''
+    
+    def append(self, data: Union[str, bytes]):
+        if isinstance(data, str):
+            data = bytes(data, 'utf-8')
+        self.buffer += data
+    
+    def readln(self):
+        ln = None
+        if b'\n' in self.buffer:
+            ln, self.buffer = self.buffer.split(b'\n', maxsplit=1)
+        return str(ln)
+    
+    def size(self) -> int:
+        return len(self.buffer)
+    
+    def readbytes(self, count: Optional[int]) -> bytes:
+        if count is None:
+            data = self.buffer
+        else:
+            data = self.buffer[:count]
+        self.buffer = self.buffer[count:]
+        return data
+
+
+class JsonRpcClient:
+    def __init__(self, port: int, host: str = 'localhost'):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((host, port))
+        finally:
+            self.socket.close()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args, **kwargs):
+        self.socket.close()
+    
+    def call(self, method: str, id: Union[str, int], params: Union[Dict[str, Any], List[Any]]):
+        pass
+
+
+class JsonRpcServer:
+    def __init__(
+        self,
+        methods: Dict[str, Callable],
+        host: str = 'localhost',
+        port: int = 0):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((host, port))
+            self.host, self.port = self.socket.getsockname()
+            self.methods = methods
+        finally:
+            self.socket.close()
+    
+    def handle_client(self, conn: socket.socket, adr):
+        print('thread.start', adr)
+        buffer = ByteBuffer()
+        content_length = None
+        while True:
+            while content_length is None:
+                buffer.append(conn.recv(1024))
+                ln = buffer.readln()
+                if ln is not None:
+                    parts = ln.split(' ')
+                    if (not len(parts) == 2
+                        or parts[0].lower() != 'content-length:'
+                        or not parts[-1].isdigit()):
+                        print("invalid header", ln)
+                    else:
+                        content_length = int(parts[-1])
+                        print('content-length:', content_length)
+            while buffer.size() < content_length:
+                buffer.append(conn.recv(1024))
+            content = str(buffer.readbytes(content_length))
+            print('content:', content_length, content)
+            content_length = None
+        conn.close()
+        print('thread.end')
+
+    def server_forever(self, max_workers: int = None, backlog: int = None):
+        if backlog is not None:
+            self.socket.listen(backlog)
+        with thread.ThreadPoolExecutor(max_workers, 'jsonrpc-server') as executor:
+            while True:
+                conn, adr = self.socket.accept()
+                conn.recv(1024)
+                print('accepted', adr)
+                executor.submit(self.handle_client, conn, adr)
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args, **kwargs):
+        self.close()
+    
+    def close(self):
+        self.socket.close()

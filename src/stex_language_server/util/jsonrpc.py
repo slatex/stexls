c5c3644236527734fae7fb2d037af64ceb6d316f
Marian Plivelic <MarianPlivelic@gmail.com>
2019-12-08 20:54:37 +0100

    protocol classes implemented
    Former-commit-id: bfcfe14b6505a5468e468230ddf4f8fb8693e995

--- src/stex_language_server/util/jsonrpc.py
+++ src/stex_language_server/util/jsonrpc.py
@@ -1,12 +1,20 @@
 """ This module implements jsonrpc 2.0 and other related utilities.
 Specification from: https://www.jsonrpc.org/specification#overview
 """
+from __future__ import annotations
 from typing import Optional, Union, List, Dict
 import json
 import threading
 import http
 import socket
 from enum import IntEnum
+
+__all__ = [
+    'RequestMessage',
+    'NotificationMessage',
+    'ResponseMessage',
+    'ErrorObject',
+]
 
 class Message:
     ' Base message. All Messages contain the string "jsonrpc: 2.0". '
@@ -21,7 +29,7 @@ class Message:
             JSON string.
         '''
         if encoder is None:
-            return json.dumps(self.__dict__)
+            return json.dumps(self, default=lambda o: o.__dict__)
         else:
             return encoder.encode(self.__dict__)
 
@@ -40,13 +48,15 @@ class RequestMessage(Message):
             params: Parameters for the method.
                 The parameters must be in the right order if they are a list,
                 or with the correct parameter names if a dictionary.
+                The parameters should be json serializable, but you
+                can use a custom JSONEncoder in Message.serialize().
         '''
         super().__init__()
         if id is None:
             raise ValueError('RequestMessage id must not be "None"')
-        self.id = id
         self.method = method
         self.params = params
+        self.id = id
 
 
 class NotificationMessage(Message):
@@ -64,6 +74,58 @@ class NotificationMessage(Message):
 
 
 class ResponseMessage(Message):
-    ' A response message gives success or failure status in response to a request message. '
-    def __init__(self, id: Union[str, int], ):
+    ''' A response message gives success or failure status in response
+        to a request message with the same id.
+    '''
+    def __init__(self, id: Union[str, int], result: Optional[object] = None, error: Optional[ErrorObject] = None):
+        ''' Initializes a response message.
+        Parameters:
+            id: The id of the request to respond to.
+            result: Result of the method execution.
+                Result object should be json serializable or use a custom json encoder.
+                This must be "None" if the method failed.
+            error: Information about method failure. This must be "None" if the method succeeded.
+        '''
         super().__init__()
+        if ((result is not None and error is not None)
+            or (result is None and error is None)):
+            raise ValueError('Either result or error must be defined.')
+        if result is not None:
+            self.result = result
+        if error is not None:
+            self.error = error
+        self.id = id
+
+
+class ErrorObject:
+    " Gives more information in case a request method can't be executed by the server. "
+    def __init__(self, code: int, message: str, data: Optional[object] = None):
+        ''' Constructs error object.
+        Parameters:
+            code: A number that indicates the error type occured.
+                Error codes in range -32768 to -32000 are reserved by jsonrpc.
+            message: A short description of the error.
+            data: A primitive or structured value that contains additional information.
+        '''
+        self.code = code
+        self.message = message
+        if data is not None:
+            self.data = data
+
+class ErrorCodes(IntEnum):
+    ''' jsonrpc reserved error codes
+    Parse error: Invalid JSON was received by the server. An error occurred on the server while parsing the JSON text.
+    Invalid Request: The JSON sent is not a valid Request object.
+    Method not found: The method does not exist / is not available.
+    Invalid params: Invalid method parameter(s).
+    Internal error: Internal JSON-RPC error.
+    Server error: -32000 to -32099 -> Implementation defined
+    '''
+    ParseError = -32700
+    InvalidRequest = -32600
+    MethodNotFoudn = -32601
+    InvalidParams = -32602
+    InternalError = -32603
+    # ServerError = -32000 to -32099 (implementation defined)
+    FailedToSerializeJson = -32000
+

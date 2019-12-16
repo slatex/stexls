fd80b09c724f1678b6bb70f639a434e33cf175f8
Marian Plivelic <marianplivelic@gmail.com>
2019-12-16 06:11:19 +0100

    refactored utils
    Former-commit-id: 965fe4eca872b6d7f2fb21067bcc6d581d968904

--- src/stex_language_server/util/jsonrpc/core.py
+++ src/stex_language_server/util/jsonrpc/core.py
@@ -110,55 +110,6 @@ class ResponseMessage(Message):
             self.result = result
         self.id = id
 
-def validate_json(o: object) -> Optional[ResponseMessage]:
-    ''' Validates a json object.
-    Returns:
-        Nothing if the object can be as a core.Message
-        else returns a ResponseMessage with the error 
-        that can be sent back.
-    '''
-    INVALID = ResponseMessage(o.get('id'), error=ErrorObject(ErrorCodes.InvalidRequest))
-    id = 'id' in o
-    not_null = id and o['id'] is not None
-    method = 'method' in o
-    params = 'params' in o
-    result = 'result' in o
-    error = 'error' in o
-    if (
-        o.get('jsonrpc') != '2.0'
-        or (id and not isinstance(o.get('id'), (int, str)))
-        or (method and (o['method'] is None or not isinstance(o['method'], str)))
-        or (params and (o['params'] is None or not isinstance(o['params'], (list, dict))))
-        or (result and o['result'] is None)
-        or (error and (o['error'] is None or 'code' not in o['error'] or 'message' not in o:)))
-        return INVALID
-    if not (
-        or ((not_null or not id) and method and not result and not error)
-        or (id and not method and not params and result and not error)
-        or (id and not method and not params and not result and error)):
-        return INVALID
-
-
-class RestoreException(Exception):
-    ' Exception raise by restore_message if the json is not a Message. '
-
-
-def restore_message(o: object) -> Message:
-    ''' Restores the original message from a given json object.
-        Assumes that the object is valid.
-    Return:
-        Original message, assuming the input is correct.
-    '''
-    if 'method' in o:
-        if 'id' in o:
-            return RequestMessage(
-                id=o['id'], method=o['method'], params=p.get('params'))
-        else:
-            return NotificationMessage(
-                method=o['method'], params=o.get('params'))
-    else:
-        return ResponseMessage(
-            id=o.get('id'), result=o.get('result'), error=o.get('error'))
 
 
 class ErrorObject:
---
+++ src/stex_language_server/util/jsonrpc/util.py
@@ -0,0 +1,53 @@
+from typing import Optional
+from .core import Message, ResponseMessage, RequestMessage, NotificationMessage, ErrorCodes, ErrorObject
+
+__all__ = ['validate_json', 'restore_message', 'RestoreException']
+
+def validate_json(o: object) -> Optional[ResponseMessage]:
+    ''' Validates a json object.
+    Returns:
+        Nothing if the object can be as a core.Message
+        else returns a ResponseMessage with the error 
+        that can be sent back.
+    '''
+    INVALID = ResponseMessage(o.get('id'), error=ErrorObject(ErrorCodes.InvalidRequest))
+    id = 'id' in o
+    not_null = id and o['id'] is not None
+    method = 'method' in o
+    params = 'params' in o
+    result = 'result' in o
+    error = 'error' in o
+    if (
+        o.get('jsonrpc') != '2.0'
+        or (id and not isinstance(o.get('id'), (int, str)))
+        or (method and not isinstance(o['method'], str))
+        or (params and not isinstance(o['params'], (list, dict)))
+        or (error and (not isinstance(o['error'], dict) or 'code' not in o['error'] or 'message' not in o))):
+        return INVALID
+    if not (
+        ((not_null or not id) and method and not result and not error)
+        or (id and not method and not params and result and not error)
+        or (id and not method and not params and not result and error)):
+        return INVALID
+
+
+class RestoreException(Exception):
+    ' Exception raise by restore_message if the json is not a Message. '
+
+
+def restore_message(o: object) -> Message:
+    ''' Restores the original message from a given json object.
+        Assumes that the object is valid.
+    Return:
+        Original message, assuming the input is correct.
+    '''
+    if 'method' in o:
+        if 'id' in o:
+            return RequestMessage(
+                id=o['id'], method=o['method'], params=o.get('params'))
+        else:
+            return NotificationMessage(
+                method=o['method'], params=o.get('params'))
+    else:
+        return ResponseMessage(
+            id=o.get('id'), result=o.get('result'), error=o.get('error'))

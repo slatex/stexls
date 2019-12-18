1375d3d8b725395669e81630e31f870fd4ad7792
Marian Plivelic <marianplivelic@gmail.com>
2019-12-18 09:01:48 +0900

    set flag when started
    Former-commit-id: 0e6f6ad8bdde852ce2a75d9e36d352cbd756634a

--- src/stex_language_server/util/jsonrpc/tcp/client.py
+++ src/stex_language_server/util/jsonrpc/tcp/client.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import asyncio
 import logging
 from .protocol import JsonRpcProtocol
@@ -8,9 +9,12 @@ __all__ = ['Client']
 
 class Client(JsonRpcProtocol):
     async def open_connection(
-        self, host: str = 'localhost', port: int = 0):
+        self, host: str = 'localhost', port: int = 0, started: Optional[asyncio.Future] = None):
         log.info('Connecting client to %s:%i', host, port)
         reader, writer = await asyncio.open_connection(host, port)
+        if started is not None:
+            log.debug('Signaling that client opened the connection.')
+            started.set_result(True)
         try:
             await self.on_connect(reader, writer)
         finally:
--- src/stex_language_server/util/jsonrpc/tcp/server.py
+++ src/stex_language_server/util/jsonrpc/tcp/server.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import asyncio
 import logging
 from .protocol import JsonRpcProtocol
@@ -8,11 +9,14 @@ __all__ = ['Server']
 
 class Server(JsonRpcProtocol):
     async def serve_forever(
-        self, host: str = 'localhost', port: int = 0):
+        self, host: str = 'localhost', port: int = 0, started: Optional[asyncio.Future] = None):
         server = await asyncio.start_server(
             self.on_connect, host, port)
         info = server.sockets[0].getsockname()
         log.info('Started server at %s', info)
+        if started is not None:
+            log.debug('Signaling that server started.')
+            started.set_result(True)
         try:
             async with server:
                 await server.serve_forever()

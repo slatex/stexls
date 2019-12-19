47c321d67bd3879c978e24afb9d0df59e9357144
Marian Plivelic <marianplivelic@gmail.com>
2019-12-19 13:51:35 +0900

    tcp client and server working
    Former-commit-id: 1a8f2a891dc4cf32cf38c166d15eef506dfe4fe5

--- src/stex_language_server/util/jsonrpc/tcp/server.py
+++ src/stex_language_server/util/jsonrpc/tcp/server.py
@@ -10,28 +10,49 @@ __all__ = ['Server']
 class Server:
     def __init__(self, dispatcher_factory):
         self.__dispatcher_factor = dispatcher_factory
+        self._started = asyncio.Future()
+        self._running = False
+        self.connections = asyncio.Queue()
     
+    async def started(self):
+        ''' A coroutine that only returns after the server has started.
+        Returns:
+            Tuple of (host, port) on which the server is running. '''
+        return await self._started
+
     async def _connect(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         peername = writer.get_extra_info('peername')
         log.info('Server incoming connection from %s.', peername)
         protocol = JsonRpcTcpProtocol(reader, writer)
         dispatcher = self.__dispatcher_factor(protocol)
         protocol.set_dispatcher(dispatcher)
+        done = asyncio.Future()
+        await self.connections.put(done)
         try:
             await protocol.run_until_finished()
+            done.set_result(True)
+        except Exception as e:
+            log.critical('Server connection run loop to %s was interrupted by an exception.', peername)
+            done.set_exception(e)
         finally:
+            done.cancel()
             await protocol.close()
             log.info('Connection to client %s closed.', peername)
 
     async def serve_forever(
-        self, host: str = 'localhost', port: int = 0, started: Optional[asyncio.Future] = None):
-        server = await asyncio.start_server(
-            self._connect, host, port)
-        info = server.sockets[0].getsockname()
-        log.info('Started server at %s', info)
-        if started is not None:
-            log.debug('Signaling that server started.')
-            started.set_result(True)
-        async with server:
-            await server.serve_forever()
+        self, host: str = 'localhost', port: int = 0):
+        if self._running:
+            raise ValueError('Server is already running.')
+        self._running = True
+        try:
+            server = await asyncio.start_server(
+                self._connect, host, port)
+            info = server.sockets[0].getsockname()
+            log.info('Started server at %s', info)
+            self._started.set_result(info)
+            async with server:
+                await server.serve_forever()
+        finally:
+            self._started.cancel()
+            self._running = False
         log.info('Server %s serve_forever() finished.', info)

d3f2dd75ea3a90a058bdd7294f3dd8d11e51c1a1
Marian Plivelic <marianplivelic@gmail.com>
2019-12-20 09:02:41 +0900

    added exceptions
    Former-commit-id: 4f0c11bf8b1ffad15fb895a8764e4b3f37af619c

---
+++ src/stex_language_server/util/jsonrpc/exceptions.py
@@ -0,0 +1,18 @@
+
+class JsonRpcException(Exception):
+    ' Base for exceptions raised by this module. '
+
+class InternalException(JsonRpcException):
+    ' Exception raised when request returns with internal error. '
+
+class InvalidParamsException(JsonRpcException):
+    ' Exception raised when request returns with invalid params error. '
+
+class MethodNotFoundException(JsonRpcException):
+    ' Exception raised when request calls an invalid method. '
+
+class ParseErrorException(JsonRpcException):
+    ' Exception raised when the message sent to a server is invalid json. '
+
+class InvalidRequestException(JsonRpcException):
+    ' Exceptinon raised when the message sent to a server is malformed. '

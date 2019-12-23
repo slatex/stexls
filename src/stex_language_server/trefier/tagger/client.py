9ba53fdab56d3356aa7abafe1dbeecc3271aab4f
Marian Plivelic <marianplivelic@gmail.com>
2019-12-23 18:07:11 +0900

    some tagger client and server impl
    Former-commit-id: 95fe9182dd184c4b6fe795e080287272d5472f1f

--- src/stex_language_server/trefier/tagger/client.py
+++ src/stex_language_server/trefier/tagger/client.py
@@ -1,10 +1,39 @@
 ''' Implements the "Tagger Server Protocol" client. '''
+__all__ = ['TaggerClient']
+import asyncio
+import logging
+from stex_language_server.util.cli import Cli, Arg, command
+from stex_language_server.util.jsonrpc import tcp
+from stex_language_server.util.jsonrpc import dispatcher
+from stex_language_server.util.jsonrpc.hooks import request, notification
 
-import socket
-import sys
+from stex_language_server.trefier.models.seq2seq import Seq2SeqModel
 
-class Client:
-    def __init__(self, ip: str = 'localhost', port: int = 0):
-        super().__init__()
-        self.ip = ip
-        self.port = port
+log = logging.getLogger(__name__)
+
+class TaggerClient(dispatcher.Dispatcher):
+    @request
+    def load_model(self, path: str, force: bool = False) -> bool: pass
+    @request
+    def predict(self, *files: str): pass
+    @request
+    def get_info(self): pass
+
+if __name__ == '__main__':
+    @command(
+        host=Arg(default='localhost', help='IP of the server.'),
+        port=Arg(type=int, default=0, help='Port of the server.'),
+        loglevel=Arg(default='WARNING', help='Logger loglevel: DEBUG, INFO, WARNING, ERROR, CRITICAL'))
+    def client(host: str, port: int, loglevel: str = 'WARNING'):
+        logging.basicConfig(level=getattr(logging, loglevel.upper(), logging.WARNING))
+        async def async_wrapper():
+            log.info('client(%s, %i) called.', host, port)
+            dispatcher = asyncio.Future()
+            client = asyncio.create_task(
+                tcp.open_connection(TaggerClient, host=host, port=port, connection=dispatcher))
+            dispatcher = await dispatcher
+            await client 
+        return asyncio.run(async_wrapper())
+        
+    cli = Cli([client], description='Trefier tagger client program.')
+    cli.dispatch()
--- src/stex_language_server/trefier/tagger/server.py
+++ src/stex_language_server/trefier/tagger/server.py
@@ -1,9 +1,74 @@
 ''' Implements the server part of the "Tagger Server Protocol" '''
+__all__ = ['TaggerServer']
+import asyncio
+import logging
+from stex_language_server.util.cli import Cli, Arg, command
+from stex_language_server.util.jsonrpc import tcp
+from stex_language_server.util.jsonrpc import dispatcher
+from stex_language_server.util.jsonrpc.hooks import method
 
-import socket
+from stex_language_server.trefier.models.seq2seq import Seq2SeqModel
 
-class Server:
-    def __init__(self, ip: str = 'localhost', port: int = 0):
-        super().__init__()
-        self.ip = ip
-        self.port = port
+log = logging.getLogger(__name__)
+model = None
+
+class TaggerServer(dispatcher.Dispatcher):
+    @method
+    def load_model(self, path: str, force: bool = False):
+        global model
+        log.info('load_model(%s) called.', path)
+        log.debug('Attempting to load model from "%s"', path)
+        if not force and model is not None:
+            log.debug('Attempt to load a model even though one is already loaded.')
+            raise ValueError('Model already loaded.')
+        elif force:
+            log.debug('Force model load.')
+        try:
+            model = Seq2SeqModel.load(path)
+            log.debug(model.settings)
+            return True
+        except:
+            log.exception('Failed to load model from "%s"', path)
+            raise
+    
+    @method
+    def predict(self, *files: str):
+        log.info('predict() called.')
+        if model is None:
+            raise ValueError('No model loaded.')
+        log.debug('Predicting for files: %s', files)
+        try:
+            predictions = model.predict(*files)
+            log.debug('Predictions done: %s', predictions)
+            return predictions
+        except:
+            log.exception('Failed to create predictions.')
+            raise
+
+    @method
+    def get_info(self):
+        log.info('get_info() called.')
+        if model is None:
+            raise ValueError('No model loaded.')
+        log.debug('Settings are: %s', model.settings)
+        return model.settings
+
+if __name__ == '__main__':
+    @command(
+        host=Arg(default='localhost', help='Hostname to bind server to.'),
+        port=Arg(type=int, default=0, help='Port to bind server on.'),
+        loglevel=Arg(default='WARNING', help='Logger loglevel: DEBUG, INFO, WARNING, ERROR, CRITICAL'))
+    def server(host: str, port: int, loglevel: str = 'WARNING'):
+        logging.basicConfig(level=getattr(logging, loglevel.upper(), logging.WARNING))
+        async def async_wrapper():
+            log.info('server(%s, %i) called.', host, port)
+            started = asyncio.Future()
+            server = asyncio.create_task(
+                tcp.start_server(TaggerServer, host=host, port=port, started=started))
+            info = await started
+            print('{}:{}'.format(*info))
+            await server
+        return asyncio.run(async_wrapper())
+        
+    cli = Cli([server], description='Trefier tagger server program.')
+    cli.dispatch()

08682042635f99380e65c8949c0cbd0286b2e905
Marian Plivelic <marianplivelic@gmail.com>
2019-12-24 09:41:29 +0900

    added textio reader and writer
    Former-commit-id: 90a2ada62aa5c6605062bf0fd5723297407df5c5

--- src/stex_language_server/util/jsonrpc/streams.py
+++ src/stex_language_server/util/jsonrpc/streams.py
@@ -1,4 +1,7 @@
-from typing import Union, Optional, Any
+from typing import Union, Optional, Any, TextIO
+import sys
+import asyncio
+import functools
 import re
 import json
 import logging
@@ -21,6 +24,41 @@ class AsyncReaderStream:
     async def read(self, count: int) -> bytes:
         ' Reads count many bytes from stream and returns them. '
         raise NotImplementedError()
+
+
+class TextIoReaderStream(AsyncReaderStream):
+    def __init__(self, stream: TextIO = sys.stdin, encoding: str = 'utf-8', chunksize: int = -1):
+        self._stream = stream
+        self._buffer = b''
+        self._encoding = encoding
+        self._chunksize = chunksize
+    
+    async def readline(self, limit: int = -1) -> bytes:
+        loop = asyncio.get_event_loop()
+        partial = functools.partial(self._stream.readline, limit=limit)
+        line = await loop.run_in_executor(partial)
+        if not isinstance(line, bytes):
+            line = bytes(line, self._encoding)
+        return line
+    
+    async def readuntil(self, separator: bytes = b'\n') -> bytes:
+        if separator == b'\n':
+            return await self.readline()
+        while separator not in self._buffer:
+            data = await self.read(self._chunksize)
+            self._buffer += data
+        line, self._buffer = self._buffer.split(separator, maxsplit=1)
+        return line
+    
+    async def read(self, count: int = -1) -> bytes:
+        loop = asyncio.get_event_loop()
+        partial = functools.partial(self._stream.read, n=count)
+        data = await loop.run_in_executor(partial)
+        if not data:
+            raise EOFError()
+        if not isinstance(data, bytes):
+            data = bytes(data, self._encoding)
+        return data
 
 
 class JsonStreamReader:
@@ -72,6 +110,17 @@ class WriterStream:
     ' Interface for a stream that writes all bytes in data, then returns. '
     def write(self, data: bytes):
         raise NotImplementedError()
+
+
+class TextIoWriterStream(WriterStream):
+    def __init__(self, stream: TextIO = sys.stdout):
+        self._stream = stream
+
+    def write(self, data: bytes):
+        written = 0
+        while written < len(data):
+            written += self._stream.write(data[written:])
+
 
 
 class JsonStreamWriter:

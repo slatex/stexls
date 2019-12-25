707be4820f164c85eab384881f48827fb74ae075
Marian Plivelic <marianplivelic@gmail.com>
2019-12-25 12:59:28 +0900

    interactive
    Former-commit-id: 9a777852746cc4f376044509d890357a367f5fa4

--- src/stex_language_server/trefier/tagger/client.py
+++ src/stex_language_server/trefier/tagger/client.py
@@ -19,6 +19,7 @@ class TaggerClient(dispatcher.Dispatcher):
     @request
     def get_info(self): pass
 
+
 if __name__ == '__main__':
     @command(
         host=Arg(default='localhost', help='IP of the server.'),
@@ -32,7 +33,43 @@ if __name__ == '__main__':
             client = asyncio.create_task(
                 tcp.open_connection(TaggerClient, host=host, port=port, connection=dispatcher))
             dispatcher = await dispatcher
-            await client 
+            loop = asyncio.get_event_loop()
+            import shlex
+            cli2 = Cli(
+                [
+                    command(
+                        path=Arg(),
+                        force=Arg(action='store_true'))(dispatcher.load_model),
+                    command(
+                        files=Arg(nargs='*'))(dispatcher.predict),
+                    command()(dispatcher.get_info)
+                ],
+                description='Client CLI.'
+            )
+            async def input_task():
+                log.info('input task started.')
+                while True:
+                    log.debug('input task waiting for input in executor.')
+                    try:
+                        line = await loop.run_in_executor(input)
+                    except (KeyboardInterrupt, EOFError):
+                        log.info('Exiting by user input.')
+                        break
+                    if not line:
+                        log.debug('input task EOF received.')
+                        break
+                    line = line.strip()
+                    if line in ('quit', 'q', 'exit'):
+                        log.debug('Exit by user input.')
+                        break
+                    log.debug('Line received: %s', line)
+                    args = shlex.split(line)
+                    try:
+                        print(await cli2.dispatch(args))
+                    except:
+                        log.exception('dispatched command raised exception.')
+                log.info('input task finished.')
+            await asyncio.gather(client, input_task())
         return asyncio.run(async_wrapper())
         
     cli = Cli([client], description='Trefier tagger client program.')

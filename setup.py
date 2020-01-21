c771ee64582967a1dc88f2beb4637d39baaeefa3
Marian Plivelic <marianplivelic@gmail.com>
2020-01-21 20:25:35 +0900

    moved all for setup.py
    Former-commit-id: 8dd3eb403fec4d5adfb6d1dce38e04eb9b882a3a
    Former-commit-id: 0b751bfdfc8dd39dcebf87953f491dc02fef0a38

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stexls", # Replace with your own username
+    version="0.0.1",
+    author="Marian Plivelic",
+    author_email="MarianPlivelic@gmail.com",
+    description="Language server and other utilities for STex.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gl.kwarc.info/Marian6814/trefier-backend",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'antlr4-python3-runtime',
+        'tensorflow-gpu >= 1.15',
+        'scikit-learn',
+    ]
+)

94f39b7815e2ee88692b466980e2892d7f5235d2
Marian Plivelic <MarianPlivelic@gmail.com>
2020-03-06 15:43:54 +0100

    Link class
    Former-commit-id: 21adcdf46279ba55da4c5f9635ea8f85f0ecb682
    Former-commit-id: c8b8c38757814e02a693d46b5de491b54cc451f3

--- stexls/compiler/linker.py
+++ stexls/compiler/linker.py
@@ -1,6 +1,20 @@
 """ Linker for stex objects.
 """
 from stexls.compiler.objects import StexObject
+from stexls.compiler.symbols import Symbol
+from stexls.util.location import Location
+
+class Link:
+    def __init__(self, symbol: Symbol, location: Location):
+        """ Links the symbol with the location.
+        The symbol contains its own location of definition and
+        the link specifies a location to which the definition
+        points to.
+
+        Like a Tag in a tagfile.
+        """
+        self.symbol = symbol
+        self.location = location
 
 class Linker:
     def link(self, object: StexObject):

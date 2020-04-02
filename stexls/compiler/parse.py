c11def8f3ae06d393fe1fb73ee5311e5b7076d5c
Marian Plivelic <MarianPlivelic@gmail.com>
2020-04-02 12:32:34 +0200

    return Generator
    Former-commit-id: 452170822b1073f3a93332f5ccf7d3e469170b94
    Former-commit-id: ad24cace1aff215ae08a64e6d471b8e81d128608

--- stexls/compiler/parse.py
+++ stexls/compiler/parse.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Tuple, List, Dict, Set, Iterator
+from typing import Optional, Tuple, List, Dict, Set, Generator
 import re
 import multiprocessing
 from collections import defaultdict
@@ -45,7 +45,7 @@ class ParsedFile:
         self.errors: Dict[Location, List[Exception]] = defaultdict(list)
 
     @property
-    def split_modules(self) -> Iterator[ParsedFile]:
+    def split_modules(self) -> Generator[ParsedFile]:
         """ Splits the file into multiple ParsedFiles, one for each <module> contained. """
         if self.modules:
             for module in self.modules:

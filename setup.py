a46bfeccb4bbe74a266f4f7a2107d9e1a5d3f472
Marian Plivelic <MarianPlivelic@gmail.com>
2020-04-30 16:59:47 +0200

    3.0.2
    Former-commit-id: 8c77a710279f657cc64801d585396a0757d068f6
    Former-commit-id: 06aae58e252939e7e4653104bdbd9ce26d482151

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stexls",
-    version="3.0.1",
+    version="3.0.2",
     author="Marian Plivelic",
     author_email="MarianPlivelic@gmail.com",
     description="Language server and other utilities for STex.",

a8fb3abd754762ce56c121b7cb544d481150fe65
Marian Plivelic <marianplivelic@gmail.com>
2020-10-11 09:54:19 +0200

    added version tracking
    Former-commit-id: 51ca9c0bae507636bc1a2e8b253b45cb05c3aad2
    Former-commit-id: a22d69a43f148af4e014f35103863465a6346eb2

--- stexls/util/workspace.py
+++ stexls/util/workspace.py
@@ -12,6 +12,20 @@ log = logging.getLogger(__name__)
 
 __all__ = ['Workspace']
 
+class TextDocument:
+    def __init__(self, path: Path, version: int, text: str) -> None:
+        self.path: Path = path
+        self.version: int = version
+        self.text: str = text
+        self.time_modified: float = time.time()
+
+    def update(self, version: int, text: str):
+        ' Updates the version, text and time modified timestamp of this text document. '
+        self.version = version
+        self.text = text
+        self.time_modified = time.time()
+
+
 class Workspace:
     def __init__(self, root: Path):
         """ Opens a workspace with the specified root as root.
@@ -21,7 +35,7 @@ class Workspace:
         """
         self.root = Path(root).expanduser().resolve().absolute()
         # Map of files to a tuple of file time modified and content
-        self._open_files: Dict[Path, (float, str)] = {}
+        self._open_files: Dict[Path, TextDocument] = {}
         self._ignore: Optional[List[Pattern]] = None
         self._include: Optional[List[Pattern]] = None
 
@@ -29,20 +43,27 @@ class Workspace:
         ' Returns true if a modified version of this file that can be queried with read_file() is in memory. '
         return file in self._open_files
 
+    def get_version(self, file: Path) -> Optional[int]:
+        ' Returns the version of the file if it is added. '
+        if self.is_open(file):
+            return self._open_files[file].version
+        return None
+
     def get_time_buffer_modified(self, file: Path) -> float:
         ' Retrieves the timestamp since the last edit to the buffered file. Returns 0 if the file is not open. '
-        mtime, _content = self._open_files.get(file, (0, None))
-        return mtime
+        if self.is_open(file):
+            return self._open_files[file].time_modified
+        return 0
 
     def get_time_modified(self, file: Path) -> float:
-        ' Get the time the file was last modified either in buffer or on disk. '
-        if file in self._open_files:
+        ' Get the time the file was last modified either in buffer or on disk. 0 if the file is not open. '
+        if self.is_open(file):
             return self.get_time_buffer_modified()
         if file.is_file():
             return file.lstat().st_mtime
         return 0
 
-    def open_file(self, path: Path, content: str) -> bool:
+    def open_file(self, path: Path, version: str, text: str) -> bool:
         """ Opens a file in the current workspace.
 
             This is needed to be able to handle files which are not saved to disk.
@@ -51,37 +72,35 @@ class Workspace:
 
         Parameters:
             path: Path of the opened file.
-            content: Content of the file when opening it.
+            version: Text document version identifier provided by the language client.
+            text: Content of the file when opening it.
 
         Returns:
-            True if the file was opened or is already open. False otherwise.
+            True if the file was successfully opened.
         """
         if path in self._open_files:
-            log.warning('Opened already open file: "%s"', path)
+            log.warning('File already open: "%s"', path)
+            return False
         else:
             log.debug('Opening file: "%s"', path)
         if path not in self.files:
-            log.debug('Ignoring open file attempt of "%s" because it is not part of this workspace.', path)
+            log.warning('Ignoring open file attempt of "%s" because it is not part of this workspace.', path)
             return False
-        self._open_files[path] = (time.time(), content)
+        self._open_files[path] = TextDocument(path, version, text)
         return True
 
-    def update_file_incremental(self, path: Path, content: str):
-        """ Incremental update of an opened file.
-
-            This is an optimization provided by the language server protocol
-            The client can decide to only report changes to the file and
-            the workspace constructs the modified file's state.
-            This allows the server to work with files not stored to disk.
-
-        Parameters:
-            path: The path of the file.
-        """
-        if path in self._open_files:
-            log.debug('Updating file: "%s"', path)
-        else:
-            log.warning('Updating not open file: "%s"', path)
-        self._open_files[path] = (time.time(), content)
+    def update_file(self, path: Path, version: str, text: str) -> bool:
+        ' Updates the time_modified, text and version of an already added file. Returns True on success. '
+        if not self.is_open(path):
+            log.warning('Unable to update file that has not been opened: "%s"', path)
+            return False
+        document = self._open_files[path]
+        if version < document.version:
+            log.warning('Ignoring file update with lower version number: %i < %i', version, document.version)
+            return False
+        log.debug('Updating version of "%s": from %i to %i', path, document.version, version)
+        document.update(version, text)
+        return True
 
     def close_file(self, path: Path) -> bool:
         """ Removes an opened file from ram.
@@ -112,10 +131,10 @@ class Workspace:
             Content of the file. Read from disk if not opened and read from ram if opened.
             If any kind of exception occurs None is returned.
         """
-        if path in self._open_files:
-            log.debug('Reading open file: "%s"', path)
-            _mtime, content = self._open_files[path]
-            return content
+        if self.is_open(path):
+            document = self._open_files[path]
+            log.debug('Reading open file version %i: "%s"', document.version, path)
+            return document.text
         try:
             with open(path) as fd:
                 log.debug('Reading local file: "%s"', path)

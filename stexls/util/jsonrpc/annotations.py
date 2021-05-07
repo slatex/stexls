81c3e68273ca7a19f79b35e91c8da516304be01e
Marian Plivelic <MarianPlivelic@gmail.com>
2021-05-07 16:25:12 +0200

    added recursive annotation parser for later

---
+++ stexls/util/jsonrpc/annotations.py
@@ -0,0 +1,74 @@
+from inspect import Parameter
+from typing import Literal, get_origin, get_args, Any, Union
+
+
+class JsonToPyFromAnnotationConstructor:
+    def __init__(self, annotation: Any, constructor_member_name: str = 'from_json') -> None:
+        self.annotation = annotation
+        self.constructor_member_name = constructor_member_name
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.construct(*args, **kwds)
+
+    def construct(self, value: Any) -> Any:
+        # Parse the special constructor function
+        if hasattr(self.annotation, self.constructor_member_name):
+            return getattr(self.annotation, self.constructor_member_name)(value)
+        # Return by value if no annotation given
+        if self.annotation in (Any, Parameter.empty, ...):
+            return value
+        # Assert type of primitive json values
+        # None, str, int, float can be directly parsed by json and can be type checked
+        if type(None) is self.annotation:
+            if not isinstance(value, self.annotation):
+                raise ValueError(value)
+            return value
+        if self.annotation is str:
+            if not isinstance(value, str):
+                raise ValueError(value)
+            return value
+        if self.annotation in (int, float):
+            if not isinstance(value, (int, float)):
+                raise ValueError(value)
+            return value
+        origin = get_origin(self.annotation)
+        if origin in (tuple, list):
+            args = get_args(self.annotation)
+            if args[-1] is ...:
+                constructor = JsonToPyFromAnnotationConstructor(args[-2])
+                return origin(map(constructor.construct, value))
+            return origin(
+                JsonToPyFromAnnotationConstructor(arg).construct(value[i])
+                for i, arg
+                in enumerate(args)
+            )
+        elif origin is dict:
+            key_arg, value_arg = get_args(self.annotation)
+            key_ctor = JsonToPyFromAnnotationConstructor(key_arg)
+            value_ctor = JsonToPyFromAnnotationConstructor(value_arg)
+            return {
+                key_ctor.construct(key_value): value_ctor.construct(value_value)
+                for key_value, value_value
+                in value.items()
+            }
+        elif origin is Union:
+            for arg in get_args(self.annotation):
+                try:
+                    ctor = JsonToPyFromAnnotationConstructor(arg)
+                    parsed = ctor.construct(value)
+                except Exception:
+                    continue
+                return parsed
+            raise ValueError(value)
+        elif origin is Literal:
+            for arg in get_args(self.annotation):
+                try:
+                    parsed = JsonToPyFromAnnotationConstructor(
+                        type(arg)).construct(value)
+                    if arg != parsed:
+                        continue
+                    return parsed
+                except Exception:
+                    pass
+            raise ValueError(value)
+        return self.annotation(value)
---
+++ tests/test_annotations.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from typing import Dict, List, Literal, Optional, Tuple, Union
+from unittest import TestCase
+
+from stexls import vscode
+from stexls.util.jsonrpc.annotations import JsonToPyFromAnnotationConstructor
+
+
+class TestAnnotations(TestCase):
+    def test_union(self):
+        ctor = JsonToPyFromAnnotationConstructor(Union[float, str])
+        self.assertIsInstance(ctor('string'), str)
+        self.assertIsInstance(ctor(-0.1), float)
+        self.assertIsInstance(ctor(1.0), float)
+
+    def test_dict(self):
+        ctor = JsonToPyFromAnnotationConstructor(
+            Dict[Union[float, str], Optional[Tuple[str, ...]]])
+        json = {1.0: ('a', 'b'), 'string': None}
+        self.assertDictEqual(ctor(json), json)
+        self.assertRaises(Exception, ctor, {(1, 2): (3, 4)})
+
+    def test_vscode(self):
+        file_uri = Path('some/file.tex').absolute().as_uri()
+        start = vscode.Position(1, 2)
+        stop = vscode.Position(3, 4)
+        loc = vscode.Location(file_uri, vscode.Range(start, stop))
+        json = loc.to_json()
+        ctor = JsonToPyFromAnnotationConstructor(vscode.Location)
+        self.assertIsInstance(ctor(json), vscode.Location)
+        self.assertDictEqual(ctor(json).to_json(), json)
+
+    def test_compound_vscode(self):
+        file_uri = Path('some/file.tex').absolute().as_uri()
+        start = vscode.Position(1, 2)
+        stop = vscode.Position(3, 4)
+        loc = vscode.Location(file_uri, vscode.Range(start, stop)).to_json()
+        ctor = JsonToPyFromAnnotationConstructor(
+            Optional[List[vscode.Location]])
+        self.assertIsInstance(ctor(None), type(None))
+        self.assertIsInstance(ctor((loc, loc)), list)
+        self.assertDictEqual(ctor((loc, loc))[0].to_json(), loc)
+
+    def test_option(self):
+        octor = JsonToPyFromAnnotationConstructor(
+            Optional[Union[int, float, str]])
+        self.assertIs(octor(None), None)
+        self.assertIsInstance(octor(1), int)
+        self.assertIsInstance(octor(1.5), float)
+        self.assertIsInstance(octor('str'), str)
+        self.assertRaises(ValueError, octor, (1, 2, 3))
+        ctor = JsonToPyFromAnnotationConstructor(Union[int, str])
+        self.assertRaises(ValueError, ctor, None)
+        self.assertIsInstance(ctor(1), int)
+        self.assertIsInstance(ctor(1.0), float)
+        self.assertIsInstance(ctor('str'), str)
+
+    def test_tuple(self):
+        ctor = JsonToPyFromAnnotationConstructor(
+            Dict[str, Tuple[vscode.Position, vscode.Location]])
+        start = vscode.Position(1, 2)
+        stop = vscode.Position(3, 4)
+        range = vscode.Range(start, stop)
+        uri = Path('some/file.tex').absolute().as_uri()
+        location = vscode.Location(uri, range)
+        json = {'x': (start.to_json(), location.to_json())}
+        reconstructed = ctor(json)
+        self.assertIsInstance(reconstructed['x'], tuple)
+        self.assertIsInstance(reconstructed['x'][0], vscode.Position)
+        self.assertIsInstance(reconstructed['x'][1], vscode.Location)
+        json2 = {'x': start.to_json()}
+        self.assertRaises(Exception, ctor, json2)
+
+    def test_fail_none(self):
+        ctor = JsonToPyFromAnnotationConstructor(int)
+        self.assertRaises(Exception, ctor, None)
+
+    def test_literal(self):
+        ctor = JsonToPyFromAnnotationConstructor(
+            Union[Literal[None, 1, 2, 'string'], vscode.Position])
+        self.assertIs(ctor(None), None)
+        self.assertEqual(ctor(1), 1)
+        self.assertEqual(ctor(2), 2)
+        self.assertEqual(ctor('string'), 'string')
+        pos = vscode.Position(1, 2).to_json()
+        self.assertIsInstance(ctor(pos), vscode.Position)
+        self.assertDictEqual(ctor(pos).to_json(), pos)
+        self.assertRaises(ValueError, ctor, 0)
+        self.assertRaises(ValueError, ctor, 3)
+        self.assertRaises(ValueError, ctor, 'other string')

0bc7940b3438b8d3a6aebb9ab71fc01d95a57c4e
Marian Plivelic <MarianPlivelic@gmail.com>
2021-05-10 11:22:47 +0200

    tested all errors and initialize

--- tests/test_lsp.py
+++ tests/test_lsp.py
@@ -1,18 +1,147 @@
 import asyncio
 import json
 import random
+import tempfile
 from asyncio.streams import StreamReader, StreamWriter
+from pathlib import Path
 from unittest import IsolatedAsyncioTestCase
 
+from stexls.jsonrpc.core import ErrorCodes
 from stexls.lsp.cli import lsp
 
 
 class TestLSP(IsolatedAsyncioTestCase):
-    async def test_start(self):
-        port = random.randint(2048, 65000)
+    def setUp(self) -> None:
+        self.port = random.randint(2048, 65000)
+        self.lsp = lsp(port=self.port, transport_kind='tcp')
+        self.stub = {
+            "jsonrpc": "2.0",
+            "id": "test message",
+            "method": None,
+            "params": None,
+        }
 
-        lsp_task = lsp(port=port, transport_kind='tcp')
+    async def test_initialize(self):
+        with tempfile.TemporaryDirectory() as dir:
+            async def callback(reader: StreamReader, writer: StreamWriter):
+                self.stub['id'] = 'initialize'
+                self.stub['method'] = 'initialize'
+                self.stub['params'] = {'rootUri': Path(dir).as_uri()}
+                content = json.dumps(self.stub).encode()
+                writer.write(
+                    f'content-length: {len(content)}\r\n\r\n'.encode() + content)
+                raw_content_length = await reader.readline()
+                content_length = int(
+                    raw_content_length.decode().split(':')[-1].strip())
+                self.empty_line = await reader.readline()
+                raw_response = await reader.readexactly(content_length)
+                writer.close()
+                self.response = json.loads(raw_response.decode())
+                print(self.response)
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
+        server.close()
+        self.assertIn('result', self.response)
+        result = self.response['result']
+        self.assertIn('capabilities', result)
+        self.assertIn('serverInfo', result)
+        capabilities = result['capabilities']
+        self.assertIn('textDocumentSync', capabilities)
+        self.assertTrue(capabilities['completionProvider'])
+        self.assertTrue(capabilities['definitionProvider'])
+        self.assertTrue(capabilities['referencesProvider'])
 
+    async def test_parse_error(self):
+        async def callback(reader: StreamReader, writer: StreamWriter):
+            content = b'{ fail to parse'
+            writer.write(
+                f'content-length: {len(content)}\r\n\r\n'.encode() + content)
+            raw_content_length = await reader.readline()
+            content_length = int(
+                raw_content_length.decode().split(':')[-1].strip())
+            self.empty_line = await reader.readline()
+            raw_response = await reader.readexactly(content_length)
+            writer.close()
+            self.response = json.loads(raw_response.decode())
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
+        server.close()
+        self.assertEqual(self.empty_line, b'\r\n')
+        self.assertIn('error', self.response)
+        self.assertIn('code', self.response['error'])
+        self.assertEqual(ErrorCodes.ParseError, self.response['error']['code'])
+
+    async def test_internal_error(self):
+        # If initialize is called without a `root` param, it will raise, causing an InternalError
+        async def callback(reader: StreamReader, writer: StreamWriter):
+            self.stub['method'] = 'initialize'
+            self.stub['params'] = {}
+            content = json.dumps(self.stub).encode()
+            writer.write(
+                f'content-length: {len(content)}\r\n\r\n'.encode() + content)
+            raw_content_length = await reader.readline()
+            content_length = int(
+                raw_content_length.decode().split(':')[-1].strip())
+            self.empty_line = await reader.readline()
+            raw_response = await reader.readexactly(content_length)
+            writer.close()
+            self.response = json.loads(raw_response.decode())
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
+        server.close()
+        self.assertEqual(self.empty_line, b'\r\n')
+        self.assertIn('error', self.response)
+        self.assertIn('code', self.response['error'])
+        self.assertEqual(ErrorCodes.InternalError,
+                         self.response['error']['code'])
+
+    async def test_invalid_params(self):
+        # the `initialized` method does not take any parameters: Giving it one will fail.
+        async def callback(reader: StreamReader, writer: StreamWriter):
+            self.stub['method'] = 'initialized'
+            self.stub['params'] = {'invalid param': 0}
+            content = json.dumps(self.stub).encode()
+            writer.write(
+                f'content-length: {len(content)}\r\n\r\n'.encode() + content)
+            raw_content_length = await reader.readline()
+            content_length = int(
+                raw_content_length.decode().split(':')[-1].strip())
+            self.empty_line = await reader.readline()
+            raw_response = await reader.readexactly(content_length)
+            writer.close()
+            self.response = json.loads(raw_response.decode())
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
+        server.close()
+        self.assertEqual(self.empty_line, b'\r\n')
+        self.assertIn('error', self.response)
+        self.assertIn('code', self.response['error'])
+        self.assertEqual(ErrorCodes.InvalidParams,
+                         self.response['error']['code'])
+
+    async def test_method_not_found(self):
+        async def callback(reader: StreamReader, writer: StreamWriter):
+            self.stub['method'] = 'this method does not exist'
+            content = json.dumps(self.stub).encode()
+            writer.write(
+                f'content-length: {len(content)}\r\n\r\n'.encode() + content)
+            raw_content_length = await reader.readline()
+            content_length = int(
+                raw_content_length.decode().split(':')[-1].strip())
+            self.empty_line = await reader.readline()
+            raw_response = await reader.readexactly(content_length)
+            writer.close()
+            self.response = json.loads(raw_response.decode())
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
+        server.close()
+        self.assertEqual(self.empty_line, b'\r\n')
+        self.assertIn('error', self.response)
+        self.assertIn('code', self.response['error'])
+        self.assertEqual(ErrorCodes.MethodNotFound,
+                         self.response['error']['code'])
+
+    async def test_invalid_request(self):
         async def callback(reader: StreamReader, writer: StreamWriter):
             content = json.dumps(
                 {'message': 'Hello, World!'}).encode()
@@ -21,16 +150,15 @@ class TestLSP(IsolatedAsyncioTestCase):
             raw_content_length = await reader.readline()
             content_length = int(
                 raw_content_length.decode().split(':')[-1].strip())
-            self.assertEqual(content_length, 87)
-            empty_line = await reader.readline()
-            self.assertEqual(empty_line, b'\r\n')
-            response = await reader.readexactly(content_length)
-            obj = json.loads(response.decode())
-            expected_response = {
-                'error': {'code': -32600, 'message': 'Invalid Request'},
-                'id': None, 'jsonrpc': '2.0'}
-            self.assertDictEqual(expected_response, obj)
+            self.empty_line = await reader.readline()
+            raw_response = await reader.readexactly(content_length)
+            self.response = json.loads(raw_response.decode())
             writer.close()
-        server = await asyncio.start_server(callback, host='localhost', port=port)
-        await lsp_task
+        server = await asyncio.start_server(callback, host='localhost', port=self.port)
+        await self.lsp
         server.close()
+        self.assertEqual(self.empty_line, b'\r\n')
+        expected_response = {
+            'error': {'code': ErrorCodes.InvalidRequest, 'message': 'Invalid Request'},
+            'id': None, 'jsonrpc': '2.0'}
+        self.assertDictEqual(expected_response, self.response)

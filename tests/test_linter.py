fa8875c13872b45c155e2ba69efe71514d7816dc
Marian Plivelic <MarianPlivelic@gmail.com>
2021-05-10 09:55:08 +0200

    need to fix smglom tests

--- tests/test_linter.py
+++ tests/test_linter.py
@@ -18,6 +18,37 @@ class TestLinter(TestCase, MockGlossary):
     def tearDown(self) -> None:
         self.cleanup()
 
+    def test_ok_smglom(self):
+        module = self.module_name
+        self.write_modsig(r'''
+            \symi{symbol1}
+            \symii{symbol}{2}
+            \symdef{symdef}
+        ''')
+        self.write_binding(r'''
+            \trefi{symbol1}
+            \trefii{symbol}{2}
+            \adefii{symdef}{defines}{symdef}
+        ''')
+        self.new_module('module2')
+        self.write_modsig(rf'''
+            \gimport{{{module}}}
+            \symiv{{symbol}}{{in}}{{new}}{{module}}
+            \symdef[noverb]{{does-not-have-a-trefi}}
+        ''')
+        self.write_binding(rf'''
+            \trefi[{module}]{{symbol1}}
+            \trefii[{module}]{{symbol}}{{2}}
+            \trefi{{symdef}}
+            \defi[name=symbol-in-new-module]{{define using 'name' argument}}
+            \atrefi{{symbol-in-new-module}}{{trefi by using the name with '-' directly in 'a' rarg}}
+        ''')
+        compile_iter = self.linter.compile_workspace()
+        compiled_file_paths = list(compile_iter)
+        for file in compiled_file_paths:
+            result = self.linter.lint(file)
+            self.assertListEqual([], result.diagnostics)
+
     def test_lint(self):
         self.write_modsig(r'''\symi{value}\symii{error}''')
         self.write_binding(r'''

00a04a507b332d8fed8314f8399a739b56b4e8c8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 18:25:24 +0000

    Adding tarfile member sanitization to extractall()

--- stexls/util/download.py
+++ stexls/util/download.py
@@ -189,7 +189,26 @@ def maybe_download_and_extract(
                 with tarfile.open(path_to_save_location, "r:gz") as tar_ref:
                     sprint("Extracting .tar.gz file to %s..." %
                            path_to_extract_location)
-                    tar_ref.extractall(path_to_extract_location)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar_ref, path_to_extract_location)
             elif url.endswith(".gz"):
                 with gzip.open(path_to_save_location, 'rb') as gz_ref, open(path_to_extract_location, 'wb') as out_ref:
                     sprint("Extracting .gz file to %s..." %
